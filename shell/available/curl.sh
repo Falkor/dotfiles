ee2878668da873103f49a6b764a218bc5a23236c
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2017-01-26 16:38:53 +0100

    curl install
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

---
+++ shell/available/curl.sh
@@ -0,0 +1,4 @@
+# Customization for curl, assuming XDG Base Directory setup
+# See https://specifications.freedesktop.org/basedir-spec/latest/
+# Place this file in the custom directory settings for your favorite shell 
+export CURL_HOME="$XDG_CONFIG_HOME/curl"
