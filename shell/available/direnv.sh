c88357d5e128a56a48e1a0d5843e73ec35e4ff9b
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2020-05-06 21:48:27 +0200

    PS1 restore
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

--- shell/available/direnv.sh
+++ shell/available/direnv.sh
@@ -4,4 +4,13 @@
 if [ -n "$(which direnv 2>/dev/null)" ]; then
   eval "$(direnv hook $(basename $SHELL))"
   # export DIRENV_WARN_TIMEOUT=100s
+
+  # See https://github.com/direnv/direnv/wiki/Python#restoring-the-ps1
+  show_virtual_env() {
+    if [[ -n "$VIRTUAL_ENV" && -n "$DIRENV_DIR" ]]; then
+        echo "($(basename $VIRTUAL_ENV))"
+    fi
+    }
+    export -f show_virtual_env
+    PS1='$(show_virtual_env) '$PS1
 fi
