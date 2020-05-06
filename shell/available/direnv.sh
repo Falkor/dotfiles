aa60d9b26c6850c28b5db3899c1a7b1720f9a538
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2020-05-07 00:45:42 +0200

    [direnv] correct PS1 for python venv bash/zsh
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

--- shell/available/direnv.sh
+++ shell/available/direnv.sh
@@ -2,15 +2,20 @@
 # See https://direnv.net/
 
 if [ -n "$(which direnv 2>/dev/null)" ]; then
-  eval "$(direnv hook $(basename $SHELL))"
-  # export DIRENV_WARN_TIMEOUT=100s
+    eval "$(direnv hook $(basename $SHELL))"
+    # export DIRENV_WARN_TIMEOUT=100s
 
-  # See https://github.com/direnv/direnv/wiki/Python#restoring-the-ps1
-  show_virtual_env() {
-    if [[ -n "$VIRTUAL_ENV" && -n "$DIRENV_DIR" ]]; then
-        echo "($(basename $VIRTUAL_ENV))"
-    fi
+    # See https://github.com/direnv/direnv/wiki/Python#restoring-the-ps
+    show_virtual_env() {
+        if [[ -n "$VIRTUAL_ENV" && -n "$DIRENV_DIR" ]]; then
+            echo "($(basename $VIRTUAL_ENV))"
+        fi
     }
-    export -f show_virtual_env
+    if [[ -n ${ZSH_VERSION-} ]]; then
+        setopt PROMPT_SUBST
+    else
+        export -f show_virtual_env
+    fi
+
     PS1='$(show_virtual_env) '$PS1
 fi
