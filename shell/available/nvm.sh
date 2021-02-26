be4d813d71961830a03833d14c98f7688845102e
Sebastien Varrette <Sebastien.Varrette@uni.lu>
2021-02-26 16:51:55 +0100

    default config for NVM
    Signed-off-by: Sebastien Varrette <Sebastien.Varrette@uni.lu>

---
+++ shell/available/nvm.sh
@@ -0,0 +1,5 @@
+# NVM: Node Version Manager
+# See https://github.com/nvm-sh/nvm
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
