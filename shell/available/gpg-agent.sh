2ad28db7ddbbb536c7f34fbd0ccc72c6fdda7b65
Sebastien Varrette <sebastien.varrette@ssi.gouv.fr>
2023-12-10 12:51:25 +0100

    [shell] gpg-agent to enable SSH access using a GPG key for authentication
    Signed-off-by: Sebastien Varrette <sebastien.varrette@ssi.gouv.fr>

---
+++ shell/available/gpg-agent.sh
@@ -0,0 +1,15 @@
+# gpg-agent - Secret key management for GnuPG configuration
+#
+# Daemon to manage secret (private) keys independently from any protocol
+# The agent is automatically started on demand by gpg, gpgconf, or gpg-connect-agent
+# After changing the configuration, reload the agent with
+#             gpg-connect-agent reloadagent /bye
+#
+if [ -n "$(which gpg-agent 2>/dev/null)" ]; then
+    # check if ssh-support is enable in your gpg-agent config
+    if [ -n "$(grep -ve '^#' $(gpgconf --list-dirs homedir)/gpg-agent.conf | grep enable-ssh-support)" ]; then
+        export GPG_TTY=$(tty)
+        export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+        gpgconf --launch gpg-agent
+    fi
+fi
