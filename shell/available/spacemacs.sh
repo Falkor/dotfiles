8ee06fb01078baebbadfd642ba0d7675679ec3dd
Sebastien Varrette <sebastien.varrette@ssi.gouv.fr>
2025-04-07 10:41:07 +0200

    [shell] spacemacs config
    Signed-off-by: Sebastien Varrette <sebastien.varrette@ssi.gouv.fr>

---
+++ shell/available/spacemacs.sh
@@ -0,0 +1,6 @@
+# Spacemacs
+# inspired from https://practical.li/spacemacs/install-spacemacs/#clone-practicalli-spacemacs-configuration
+#
+# Set XDG location of Emacs Spacemacs configuration
+export SPACEMACSDIR="$XDG_CONFIG_HOME/spacemacs"
+
